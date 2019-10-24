87ad4e45c862858c26ebcda79ce3d91e1f414d68
Tom Bennett <thomas.bennett@williamhill.com>
2019-10-24 14:20:07 +0100

    rewritten spotify API calls to use async/await instead of promises for easier reading.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,38 +25,38 @@ const Spotify = {
     },
 
     // Uses access token to return a response from the Spoitify API using user serach term from SearchBar
-    search(term) {
+    async search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+        let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
-        }).then(
-            response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    console.log('API request failed');
-                }
-            }).then(
-                jsonResponse => {
-                    if (!jsonResponse.tracks) {
-                        return [];
-                    }
-                    return jsonResponse.tracks.items.map(track => ({
-                        id: track.id,
-                        name: track.name,
-                        artist: track.artists[0].name,
-                        album: track.album.name,
-                        uri: track.uri,
-                        cover: track.album.images[2].url,
-                        preview: track.preview_url
-                    }));
-                });
+        })
+        let jsonResponse;
+        if (response.ok) {
+            jsonResponse = await response.json();
+        } else {
+            console.log('API request failed');
+        }
+
+
+        if (!jsonResponse.tracks) {
+            return [];
+        }
+        return jsonResponse.tracks.items.map(track => ({
+            id: track.id,
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri,
+            cover: track.album.images[2].url,
+            preview: track.preview_url
+        }));
+
     },
 
     // Gets a user's ID from Spotify, creates a new playlist on user's account, and adds tracks to that playlist
-    savePlaylist(playlistName, trackURIs) {
+    async savePlaylist(playlistName, trackURIs) {
         if (!playlistName || !trackURIs.length) {
             return;
         }
@@ -67,41 +67,36 @@ const Spotify = {
         let userId;
 
         // Return user's ID from Spotify API
-        return fetch('https://api.spotify.com/v1/me', {
+        let response = await fetch('https://api.spotify.com/v1/me', {
             headers: headers
-        }).then(
-            response => {
-                if (response.ok) {
-                    return response.json();
-                }
-            }).then(
-                jsonResponse => {
-                    userId = jsonResponse.id;
+        })
+        let jsonResponse;
+        if (response.ok) {
+            jsonResponse = await response.json();
+        }
 
-                    // Adds playlist to user's account
-                    return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-                        headers: headers,
-                        method: 'POST',
-                        body: JSON.stringify({ name: playlistName })
-                    }).then(
-                        response => {
-                            if (response.ok) {
-                                return response.json();
-                            } else {
-                                console.log('API request failed');
-                            }
-                        }).then(
-                            jsonResponse => {
-                                const playlistId = jsonResponse.id;
+        userId = jsonResponse.id;
 
-                                // Adds tracks to new playlist 
-                                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
-                                    headers: headers,
-                                    method: 'POST',
-                                    body: JSON.stringify({ uris: trackURIs })
-                                });
-                            });
-                });
+        // Adds playlist to user's account
+        let pResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify({ name: playlistName })
+        })
+        let pJsonResponse;
+        if (pResponse.ok) {
+            pJsonResponse = await pResponse.json();
+        } else {
+            console.log('API request failed');
+        }
+        const playlistId = pJsonResponse.id;
+
+        // Adds tracks to new playlist 
+        return await fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify({ uris: trackURIs })
+        });
     }
 }
 
